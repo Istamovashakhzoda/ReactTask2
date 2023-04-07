dffd8a2086dd85eb6828ff9b5b7bd519c08c0345
Istamovashakhzoda <117287001+Istamovashakhzoda@users.noreply.github.com>
2023-04-07 10:59:42 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+import Header from "./layouts/Header";
+
+import Main from "./components/Main";
+
+import About from "./components/About";
+
+import Footer from "./layouts/Footer";
+
+import "./assets/styles/home.css";
+import "./assets/styles/header.css";
+import "./assets/styles/navbar.css";
+import "./assets/styles/about.css";
+import "./assets/styles/footer.css"
+
+function App() {
+  return (
+    <div>
+      <Header />
+       <Main />
+       <About />
+       <Footer />
+    </div>
+  );
+}
+
+export default App;
